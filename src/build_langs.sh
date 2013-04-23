1f2c8d8ff9ded5d6b119e0b0cb61f20eb338c0d2
Glenn Washburn <development@efficientek.com>
2013-04-23 19:45:51 +0700

    Support automatic generation of Languages.cpp from langtabl.txt.

--- src/build_langs.sh
+++ src/build_langs.sh
@@ -6,10 +6,30 @@ fi
 
 MO_NAME="dng4ps-2.mo"
 
-for L in "langs/"*.po; do
+# Update language table from:
+# http://trac.wxwidgets.org/browser/wxWidgets/trunk/misc/languages/langtabl.txt
+
+# Reset Languages.cpp, it will be generated here
+#~ [ ! -f "Languages.cpp" ] &&
+cp "Languages.cpp"{.in,}
+
+find "langs/" -type f -iname '*.po' | sort |
+while read L; do
     if [ -f "$L" ]; then
         LANG_CODE="$(basename "$L" '.po')"
         mkdir -p "$1/$LANG_CODE" &&
         msgfmt -D "./langs" -o "$1/$LANG_CODE/$MO_NAME" "${LANG_CODE}.po"
+        
+        # Generate 
+        grep "[a-zA-Z0-9_]\+ \+${LANG_CODE}" langtabl.txt | head -n 1 |
+        (
+            read WXLANGCODE ISONAME LANG SUBLANG DIR NAME
+            #~ echo $WXLANGCODE $ISONAME $LANG $SUBLANG $DIR $NAME
+            echo Adding $NAME language
+            
+            #~ LANGINFO_INIT="{\"$ISONAME\", $NAME, $WXLANGCODE},"
+            LANGINFO_INIT="\"${LANG_CODE}\","
+            sed -i "s#/\* GENERATE LANGTABL \*/#${LANGINFO_INIT}\n    /* GENERATE LANGTABL */#"  "Languages.cpp"
+        )
     fi
 done

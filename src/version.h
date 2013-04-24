c281c1ce4723d665d1214dca367da188bef70375
Glenn Washburn <development@efficientek.com>
2013-04-24 14:17:12 +0700

    Update version 0.2.4.0.

--- src/version.h
+++ src/version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "05";
-	static const char MONTH[] = "07";
-	static const char YEAR[] = "2009";
-	static const double UBUNTU_VERSION_STYLE = 9.07;
+	static const char DATE[] = "20";
+	static const char MONTH[] = "04";
+	static const char YEAR[] = "2013";
+	static const double UBUNTU_VERSION_STYLE = 13.04;
 	
 	//Software Status
 	static const char STATUS[] = "Beta";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 0;
 	static const long MINOR = 2;
-	static const long BUILD = 3;
-	static const long REVISION = 15;
+	static const long BUILD = 4;
+	static const long REVISION = 0;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 286;
-	#define RC_FILEVERSION 0,2,3,15
-	#define RC_FILEVERSION_STRING "0, 2, 3, 15\0"
-	static const char FULLVERSION_STRING[] = "0.2.3.15";
+	#define RC_FILEVERSION 0,2,4,0
+	#define RC_FILEVERSION_STRING "0, 2, 4, 0\0"
+	static const char FULLVERSION_STRING[] = "0.2.4.0";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;

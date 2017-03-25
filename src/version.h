987d400f350aabbf3be4a08cd9bae5525109457b
Glenn Washburn <development@efficientek.com>
2017-03-24 23:37:40 -0500

    Release 2.4.1.

--- src/version.h
+++ src/version.h
@@ -17,13 +17,13 @@ namespace AutoVersion{
 	static const long MAJOR = 0;
 	static const long MINOR = 2;
 	static const long BUILD = 4;
-	static const long REVISION = 0;
+	static const long REVISION = 1;
 	
 	//Miscellaneous Version Types
 	static const long BUILDS_COUNT = 286;
-	#define RC_FILEVERSION 0,2,4,0
-	#define RC_FILEVERSION_STRING "0, 2, 4, 0\0"
-	static const char FULLVERSION_STRING[] = "0.2.4.0";
+	#define RC_FILEVERSION 0,2,4,1
+	#define RC_FILEVERSION_STRING "0, 2, 4, 1\0"
+	static const char FULLVERSION_STRING[] = "0.2.4.1";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 0;
